0e4a572904873cfb2d91623153837d9762bb8fd4
wisjn <wiysn2@naver.com>
2021-04-27 17:14:23 +0900

    redirection 수정

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
 	public String create(User user) {
 		System.out.println("user: " + user);
 		users.add(user);
-		return "redirect:/userList";
+		return "redirect:/list";
 	}
 	
 	@GetMapping("/list")
