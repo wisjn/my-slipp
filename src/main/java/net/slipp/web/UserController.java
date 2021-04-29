723cdc88f9be5f0f93474b9545b65d4fd9b43742
wisjn <wiysn2@naver.com>
2021-04-30 02:37:15 +0900

    개인정보 수정 기능 구현 완료

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,27 +1,83 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user: " + user);
-		users.add(user);
-		return "redirect:/list";
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
+		List<User> users = userRepository.findAll();
 		model.addAttribute("users", users);
-		return "userList";
+		return 	"user/list";
 	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "user/form";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "user/updateForm";
+	}
+	
+	
+	@GetMapping("/login")
+	public String login() {
+		return "user/login";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
+		
+		return "redirect:/users";
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
