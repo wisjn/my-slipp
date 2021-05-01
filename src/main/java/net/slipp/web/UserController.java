710aad2fc734cd649149b9849a561b1b4c5ef61a
wisjn <wiysn2@naver.com>
2021-05-02 02:59:09 +0900

    로그인 기능 구현 완료

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -2,6 +2,8 @@ package net.slipp.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -51,6 +53,25 @@ public class UserController {
 	@GetMapping("/login")
 	public String login() {
 		return "user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if(user == null) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/login";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/login";
+		}			
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		
+		return "redirect:/";
 	}
 	
 	@PutMapping("/{id}")
